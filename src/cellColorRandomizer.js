6bcf8c3b1c5ca2514c225c54f2addffd88601dbf
Jeff Martin <chess.logic@gmail.com>
2025-01-19 20:34:53 -0800

    abandon importing js file - inline only

--- src/cellColorRandomizer.js
+++
@@ -1,4 +0,0 @@
-export const colors = ["sky", "pink", "purple", "emerald"]
-export const randomizeColor = () => colors[Math.floor(Math.random() * (colors.length))]
-export const randomizeHue = () => Math.floor((Math.random() * (2) + 5) + 1) * 100
-export const getColorClass = () => `bg-${randomizeColor()}-${randomizeHue()}`
