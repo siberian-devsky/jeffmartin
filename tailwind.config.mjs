359ea07fa34560ebf92395d4c9e0fea18fc05ea3
Jeff Martin <chess.logic@gmail.com>
2025-01-19 22:48:59 -0800

    The first real portfolio commit
    * Intro page with some TW-CSS styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,15 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			transitionDuration: {
+				'1200': '1200ms',
+				'1500': '1500ms',
+				'2000': '2000ms'
+			}
+		},
 	},
 	plugins: [],
-	safelist: [
-		{
-			pattern: /bg-(red|green|blue|sky|pink|purple|emerald)-(100|200|300|400|500|600|700|800|900)/,
-		},
-		{
-			pattern: /grid-(rows|cols)-[0-8]/
-		}
-	]
+	safelist: []
 }
