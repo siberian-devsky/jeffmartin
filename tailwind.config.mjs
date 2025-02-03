08dba85fa390dfdab1de3bbec7be963c1b7cd0a3
Jeff Martin <chess.logic@gmail.com>
2025-02-02 23:12:12 -0800

    UI Rejiggering
    * New color scheme
    * Fixed transitions (hopefully deploy wont break them)
    * Resize and reealign some things
    * Update responsiveness

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,14 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			boxShadow: {
+				"custom": "0px 0px 100px 0px rgba(12,197,234,0.10)",
+			},
+			fontFamily: {
+				"unifontex": 'UnifontEX',
+			},
 			transitionDuration: {
 				'1200': '1200ms',
 				'1500': '1500ms',
 				'2000': '2000ms',
 			},
-			fontFamily: {
-				"unifontex": 'UnifontEX',
-			}
 		},
 	},
 	plugins: [
